c32eca9c27b910dd73d1e696b441891263f84db7
iampranavdhar <pranavdhar.nalamalapu@myaroundly.com>
2022-01-31 23:10:19 +0530

    Production Fix

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const isProd = process.env.NODE_ENV === "production";
 
 module.exports = {
   reactStrictMode: true,
-  assetPrefix: isProd ? "/bookfinder/" : "",
+  assetPrefix: isProd ? "https://iampranavdhar.github.io/bookfinder/" : "",
   images: {
     loader: "akamai",
     path: "",
